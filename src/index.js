7c6a641acd5a623e56f496e8b5aa9de1189e55fe
Nishant <nish.best01@gmail.com>
2019-05-30 23:39:35 +0530

    Clock app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById('root')
-);
+let clock= function () {
+  return <h2>The Time No:- {new Date().toLocaleTimeString()}</h2>
+}
+setInterval(function () {
+  ReactDOM.render(
+      clock(),
+      document.getElementById('root')
+  );
+},1000);
